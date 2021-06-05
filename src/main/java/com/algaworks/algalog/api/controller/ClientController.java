f6152326611b2893428608e679696186e138e128
Gabriela Gama <gabycgama@hotmail.com>
2021-06-05 17:39:34 -0300

    Using Spring Data JPA

--- src/main/java/com/algaworks/algalog/api/controller/ClientController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClientController.java
@@ -1,32 +1,24 @@
 package com.algaworks.algalog.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Client;
+import com.algaworks.algalog.domain.repository.ClientRepository;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientController {
 
+	@Autowired
+	ClientRepository repository;
+	
 	@GetMapping
 	public List<Client> findAll() {
-		var client1 = new Client();
-		client1.setId(1L);
-		client1.setName("João");
-		client1.setPhone("34 99999-1111");
-		client1.setEmail("joaodascouves@algaworks.com");
-		
-		var client2 = new Client();
-		client2.setId(2L);
-		client2.setName("Maria");
-		client2.setPhone("11 97777-2222");
-		client2.setEmail("mariadasilva@algaworks.com");
-		
-		return Arrays.asList(client1, client2);
+		return repository.findAll();
 	}
 }
