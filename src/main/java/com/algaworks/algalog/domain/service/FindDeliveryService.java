f1b68aab05a38ada48614df64e371b715334a299
Gabriela Gama <gabycgama@hotmail.com>
2021-06-14 23:05:49 -0300

    fix: annotations

--- src/main/java/com/algaworks/algalog/domain/service/FindDeliveryService.java
+++ src/main/java/com/algaworks/algalog/domain/service/FindDeliveryService.java
@@ -1,9 +1,15 @@
 package com.algaworks.algalog.domain.service;
 
+import org.springframework.stereotype.Service;
+
 import com.algaworks.algalog.domain.exception.EntityNotFoundException;
 import com.algaworks.algalog.domain.model.Delivery;
 import com.algaworks.algalog.domain.repository.DeliveryRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
 public class FindDeliveryService {
 
 	private DeliveryRepository repository;
